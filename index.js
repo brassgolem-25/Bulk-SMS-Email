f36b3c082829ffeb5893b380e24ff0a428e64dca
Tushar <tushartiwari2002@gamil.com>
2022-10-29 15:42:27 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const http = require('https');
+const nodemailer = require('nodemailer');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+require('dotenv').config()
+app.use(express.static('public'));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "./public/index.html")
+})
+
+app.post("/",function(req,res){
+  // const ema = req.body.email1;
+  const  nme = req.body.email2;
+  const sub=req.body.subject;
+  const txt=req.body.Message;
+  const mobile = req.body.Mobile;
+  const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {  
+      user: "divyathakur20@outlook.com",
+      pass: "Divya@99"
+    }
+  })
+  var myEmails = [nme]
+  const options = {
+      from:"divyathakur20@outlook.com",
+      to:myEmails,
+      subject:sub,
+      text:txt
+  }
+  transporter.sendMail(options,function(err,info){
+      if(err){
+          console.log(err);
+          return;
+      }
+      console.log("Sent "+ info.response);
+  })
+
+  
+
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = require('twilio')(accountSid, authToken);
+  
+  client.messages 
+  .create({body: sub, from: '+13143508058', to: mobile})
+  .then(message => console.log(message.sid));
+  
+  res.sendFile(__dirname+'/success.html')
+
+})
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server up and running at port 3000.");
+})
+
+
+
